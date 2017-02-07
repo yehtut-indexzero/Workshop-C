978c8fe3c2281a5938d19939ac775492c79f5616
yehtut-indexzero <yehtut.lord@gmail.com>
2017-02-07 14:35:36 +0800

    v2

---
+++ Workshop-C/C-3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_C
+{
+    class C_3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Marks");
+
+            int marks = Convert.ToInt16(Console.ReadLine());
+
+            if (marks < 0 || marks > 100)
+            {
+                Console.WriteLine("Not Valid");
+            }
+            else if (marks <= 0 || marks <= 40)
+            {
+                Console.WriteLine("F");
+            }
+            else if (marks == 40 || marks <= 59)
+            {
+                Console.WriteLine("C");
+            }
+            else if(marks==60 || marks<=79)
+            {
+                Console.WriteLine("B");
+            }
+            else if(marks==80 || marks<=100)
+            {
+                Console.WriteLine("A");
+            }
+        }
+    }
+}
---
+++ Workshop-C/C-4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_C
+{
+    class C_4
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Distance");
+            double dist = Math.Round(Convert.ToDouble(Console.ReadLine()), 2);
+            if (dist==0 && dist==0.5)
+            {
+                Console.WriteLine("2.4");
+            }
+            else if(dist<=8.5)
+            {
+                Console.WriteLine(2.40+85*0.04);
+            }
+            else if (dist>=9)
+            {
+                Console.WriteLine(2.40 + 85 * 0.04+(122-90)*0.005);
+            }
+        }
+    }
+}
